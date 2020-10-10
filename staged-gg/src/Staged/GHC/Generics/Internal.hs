ee1dd824e410e86709f36b95b4f93472429ea239
Oleg Grenrus <oleg.grenrus@iki.fi>
2020-10-11 01:51:36 +0300

    Move staged-gg to own repository

--- staged-gg/src/Staged/GHC/Generics/Internal.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE TemplateHaskellQuotes #-}
-module Staged.GHC.Generics.Internal (sapply) where
-
-import Language.Haskell.TH (Code, Quote)
-
-sapply :: Quote q => Code q (a -> b) -> Code q a -> Code q b
-sapply cf cx = [|| $$cf $$cx ||]
