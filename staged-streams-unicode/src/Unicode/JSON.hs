6ce7fea7cd716b75fba1d4dc69d919ba4bed57a7
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-11-28 21:50:23 +0200

    Complete decoder

--- staged-streams-unicode/src/Unicode/JSON.hs
+++ staged-streams-unicode/src/Unicode/JSON.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -ddump-splices #-}
 -- | JSON string literal decoder
 module Unicode.JSON (
-    unescapeString,
+    unescapeText,
+    UnicodeException (..),
 ) where
 
-import Data.Char        (chr)
-import Data.Word        (Word32)
-import System.IO.Unsafe (unsafePerformIO)
+import Control.Exception (Exception, throwIO, try)
+import System.IO.Unsafe  (unsafePerformIO)
 
 import Staged.Commons
 
@@ -14,7 +15,6 @@ import qualified Data.ByteString        as BS
 import qualified Data.Primitive         as P
 import qualified Data.Text.Array        as T
 import qualified Data.Text.Internal     as T
-import qualified Staged.Stream.Fallible as S
 
 import Unicode.ByteString.Source
 import Unicode.JSON.Decoder
@@ -22,22 +22,33 @@ import Unicode.PrimArray.Sink
 import Unicode.UTF16.Encoder
 
 -- for prettier splice
-{-
 import Data.Bits          (shiftL, (.&.), (.|.))
-import Data.Word          (Word8, Word16)
+import Data.Word          (Word8, Word16, Word32)
 import Foreign.ForeignPtr (plusForeignPtr, withForeignPtr)
 import Foreign.Ptr        (Ptr, plusPtr)
 import Foreign.Storable   (peek)
--}
 
-unescapeString :: BS.ByteString -> String
-unescapeString bs = unsafePerformIO $
-    $$(withUnpackedByteString [|| bs ||] $ \_len w8s ->
-          S.toList [|| invalidUtf8 ||] [|| () ||]
-        $ S.map [|| \x -> chr (fromIntegral (x :: Word32)) ||]
-        $ jsonStringDecoder w8s)
-  where
-    invalidUtf8 :: IO String
-    invalidUtf8 = fail "Invalid UTF8 or JSON escape sequence"
+-------------------------------------------------------------------------------
+-- Things
+-------------------------------------------------------------------------------
+
+unescapeText :: BS.ByteString -> Either UnicodeException T.Text
+unescapeText = unsafePerformIO . try . unescapeTextIO
+
+data UnicodeException = UnicodeException deriving (Show)
+instance Exception UnicodeException
+
+invalidUtf8 :: IO T.Text
+invalidUtf8 = throwIO UnicodeException
+
+-------------------------------------------------------------------------------
+-- stream processor
+-------------------------------------------------------------------------------
+
+unescapeTextIO :: BS.ByteString -> IO T.Text
+unescapeTextIO bs =
+    $$(withUnpackedByteString [|| bs ||] $ \len w8s ->
+       sinkPrimArray [|| invalidUtf8 ||] len [|| () ||] (utf16encoder $ jsonStringDecoder w8s) $ \len' p ->
+       sreturn [|| case $$p of P.PrimArray ba -> T.Text (T.Array ba) 0 $$len' ||])
 
 
