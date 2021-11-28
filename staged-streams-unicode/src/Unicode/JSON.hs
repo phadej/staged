04d17ba9f3c17cd07c2edb0bb0b229db47adeefa
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-11-28 22:36:08 +0200

    Add unescapeBench

--- staged-streams-unicode/src/Unicode/JSON.hs
+++ staged-streams-unicode/src/Unicode/JSON.hs
@@ -11,10 +11,10 @@ import System.IO.Unsafe  (unsafePerformIO)
 
 import Staged.Commons
 
-import qualified Data.ByteString        as BS
-import qualified Data.Primitive         as P
-import qualified Data.Text.Array        as T
-import qualified Data.Text.Internal     as T
+import qualified Data.ByteString    as BS
+import qualified Data.Primitive     as P
+import qualified Data.Text.Array    as T
+import qualified Data.Text.Internal as T
 
 import Unicode.ByteString.Source
 import Unicode.JSON.Decoder
@@ -22,8 +22,8 @@ import Unicode.PrimArray.Sink
 import Unicode.UTF16.Encoder
 
 -- for prettier splice
-import Data.Bits          (shiftL, (.&.), (.|.))
-import Data.Word          (Word8, Word16, Word32)
+import Data.Bits          (shiftL, shiftR, (.&.), (.|.))
+import Data.Word          (Word16, Word32, Word8)
 import Foreign.ForeignPtr (plusForeignPtr, withForeignPtr)
 import Foreign.Ptr        (Ptr, plusPtr)
 import Foreign.Storable   (peek)
