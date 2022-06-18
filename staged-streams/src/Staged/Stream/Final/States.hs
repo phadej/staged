cfcf32c9f173cf65e1c045a57609ae8c7789d2e3
Oleg Grenrus <oleg.grenrus@iki.fi>
2022-06-18 13:59:24 +0300

    states

---
+++ staged-streams/src/Staged/Stream/Final/States.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE PolyKinds #-}
+module Staged.Stream.Final.States where
+
+import Data.Proxy     (Proxy (..))
+import Data.SOP       (SListI2, SOP (..))
+
+import qualified GHC.Generics as GHC
+
+import Data.SOP.Fn.Append
+import Data.SOP.Fn.ConcatMapAppend
+
+
+-- | For @>>>@ combinator
+data Comp xss yss code
+    = CompL (SOP code xss)
+    | CompR (SOP code xss) (SOP code yss)
+  deriving (GHC.Generic)
+
+-- | For @append@ combinator
+data App a xss yss code
+    = AppL (code a) (SOP code xss)
+    | AppR          (SOP code yss)
+  deriving (GHC.Generic)
+
+-- | For @zipWith@ combinator
+data Zip a xss yss code
+    = ZipL          (SOP code xss) (SOP code yss)
+    | ZipR (code a) (SOP code xss) (SOP code yss)
+  deriving (GHC.Generic)
+
+-- | For @alignWith" combinator
+data Align a xss yss code
+    = AlignL               (SOP code xss) (SOP code yss)
+    | AlignR      (code a) (SOP code xss) (SOP code yss)
+    | AlignDrainL          (SOP code xss)
+    | AlignDrainR                         (SOP code yss)
+  deriving (GHC.Generic)
+
+-- | For @bfsTree@ combinator
+data BFS a xss code
+    = BfsNext            (SOP code xss)
+    | BfsStep (code [a]) (SOP code xss)
+  deriving (GHC.Generic)
+
+data Drop xss code
+    = DropL (code Int) (SOP code xss)
+    | DropR            (SOP code xss)
+  deriving (GHC.Generic)
+
+-------------------------------------------------------------------------------
+-- Composition
+-------------------------------------------------------------------------------
+
+comp_SOP
+    :: forall code xss yss. (SListI2 xss, SListI2 yss)
+    => Comp xss yss code
+    -> SOP code (Append xss (ConcatMapAppend xss yss))
+comp_SOP (CompL (SOP xss)) = SOP (injLeft (prConcatMapAppend prXss prYss) xss)
+  where
+    prXss = Proxy :: Proxy xss
+    prYss = Proxy :: Proxy yss
+
+comp_SOP (CompR (SOP xss) (SOP yss))
+    = SOP
+    $ injRight prXss
+    $ concatMapAppend_NSNP xss yss
+  where
+    prXss = Proxy :: Proxy xss
+
+uncomp_SOP
+    :: forall code xss yss. (SListI2 xss, SListI2 yss)
+    => SOP code (Append xss (ConcatMapAppend xss yss))
+    -> Comp xss yss code
+uncomp_SOP (SOP sop) = case split_NS' prXss (prConcatMapAppend prXss prYss) sop of
+    Left xss   -> CompL (SOP xss)
+    Right sop' -> case unconcatMapAppend_NSNP sop' of
+        (xss, yss) -> CompR (SOP xss) (SOP yss)
+  where
+    prXss = Proxy :: Proxy xss
+    prYss = Proxy :: Proxy yss
--- staged-streams/src/Staged/Stream/Final/Type.hs
+++ staged-streams/src/Staged/Stream/Final/Type.hs
@@ -20,11 +20,11 @@ import Data.Proxy (Proxy (..))
 import Data.SOP   (NP (..), NS (..), SListI2, SOP (..))
 
 import qualified Control.Category as C
-import qualified GHC.Generics     as GHC
 
 import Data.SOP.Fn.Append
 import Data.SOP.Fn.ConcatMapAppend
 import Staged.Stream.Step
+import Staged.Stream.Final.States
 
 -------------------------------------------------------------------------------
 -- Type definition
@@ -77,12 +77,12 @@ compose' x0 stepsX0 y0 stepsY0
     prYss = Proxy :: Proxy yss
 
     z0 :: code a -> SOP code (Append xss (ConcatMapAppend xss yss))
-    z0 = comp_SOP . CompL @code @xss @yss . x0
+    z0 = comp_SOP . CompL @xss @yss @code . x0
 
     go0 :: SOP code (Append xss (ConcatMapAppend xss yss)) -> (Step (code c) (SOP code (Append xss (ConcatMapAppend xss yss))) -> code r) -> code r
     go0 sop k = go1 (uncomp_SOP sop) $ k . fmap comp_SOP
 
-    go1 :: Comp code xss yss -> (Step (code c) (Comp code xss yss) -> code r) -> code r
+    go1 :: Comp xss yss code -> (Step (code c) (Comp xss yss code) -> code r) -> code r
     go1 (CompL xss) k = stepsX0 xss $ \case
         Stop        -> k Stop
         Skip   xss' -> k (Skip (CompL xss'))
@@ -92,42 +92,3 @@ compose' x0 stepsX0 y0 stepsY0
         Stop        -> k (Skip   (CompL xss))
         Skip   yss' -> k (Skip   (CompR xss yss'))
         Emit c yss' -> k (Emit c (CompR xss yss'))
-
--------------------------------------------------------------------------------
--- Composition
--------------------------------------------------------------------------------
-
--- TODO: copy of what is in Staged.Streams.States
-
-data Comp code xss yss
-    = CompL (SOP code xss)
-    | CompR (SOP code xss) (SOP code yss)
-  deriving (GHC.Generic)
-
-comp_SOP
-    :: forall code xss yss. (SListI2 xss, SListI2 yss)
-    => Comp code xss yss
-    -> SOP code (Append xss (ConcatMapAppend xss yss))
-comp_SOP (CompL (SOP xss)) = SOP (injLeft (prConcatMapAppend prXss prYss) xss)
-  where
-    prXss = Proxy :: Proxy xss
-    prYss = Proxy :: Proxy yss
-
-comp_SOP (CompR (SOP xss) (SOP yss))
-    = SOP
-    $ injRight prXss
-    $ concatMapAppend_NSNP xss yss
-  where
-    prXss = Proxy :: Proxy xss
-
-uncomp_SOP
-    :: forall code xss yss. (SListI2 xss, SListI2 yss)
-    => SOP code (Append xss (ConcatMapAppend xss yss))
-    -> Comp code xss yss
-uncomp_SOP (SOP sop) = case split_NS' prXss (prConcatMapAppend prXss prYss) sop of
-    Left xss   -> CompL (SOP xss)
-    Right sop' -> case unconcatMapAppend_NSNP sop' of
-        (xss, yss) -> CompR (SOP xss) (SOP yss)
-  where
-    prXss = Proxy :: Proxy xss
-    prYss = Proxy :: Proxy yss
